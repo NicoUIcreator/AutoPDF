856f6d2e3386c9881d697b6724f391b49d5cacb9
Nico <nico.coronel@hotmail.es>
2025-02-02 21:08:36 +0100

    posi tabla

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ with tab1:
 
     try:
         # Intentar cargar la imagen localmente
-        st.image("AutoPDF_transparent-.png", caption="Logo de Colaboring Barcelona SL", width=400)
+        st.image("logo.png", caption="Logo de Colaboring Barcelona SL", width=400)
     except Exception as e:
         # Si falla, cargar una imagen desde una URL pública
-        st.image("AutoPDF-.png", caption="Logo de Colaboring Barcelona SL", width=150)
+        st.image("logo.png", caption="Logo de Colaboring Barcelona SL", width=150)
 
     st.markdown("""
     <p style='text-align: center;'>
